39afd9742b7031a25801a4453dc29f24b76434b5
DennisLund <Dennis.l.christiansen@gmail.com>
2019-09-01 16:11:52 +0200

    Update mispFeed.py

--- memcacheScripts/mispFeed.py
+++ memcacheScripts/mispFeed.py
@@ -14,23 +14,36 @@ class mispToMemcache():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
     client = Client(('127.0.0.1', 11211))
-    mispKey='<MISP API-KEY>'
+    mispKey='hwwcBfywoEUHQC8Mf097y8DXqWE1k3mUXLIbdLsZ'
     dataTypes={'domain', 'ip-%'}
 
     for dt in dataTypes:
-      headers={'Authorization':mispKey,'Accept':'application/json','Content-type':'application/json'}
-      data=json.dumps({"returnFormat":"json","type":dt,"tags":"Feed-%","to_ids":"yes","includeEventTags":"yes","includeContext":"yes"})
+      headers={'Authorization':mispKey,'Accept':'application/json','Content-type                                                                                      ':'application/json'}
+      data=json.dumps({"returnFormat":"json","type":dt,"tags":"Feed-%","to_ids":                                                                                      "yes","includeEventTags":"yes","includeContext":"yes"})
       try:
-        response = requests.post('https://192.168.0.13/attributes/restSearch',headers=headers,data=data,verify=False)
+        response = requests.post('https://192.168.0.13/attributes/restSearch',he                                                                                      aders=headers,data=data,verify=False)
         data=response.json()
-        if data:
+        if(data):
           for item in data["response"]["Attribute"]:
-            client.set(str('misp-' + item['type'] + '-' + item['value']), 'misp-' + item['type'], 150)
+            valueCheck=client.get(item['value'])
+            if(valueCheck):
+              valuearr=[]
+              temparr=[]
+              mispvalue='misp-' + item['type']
+              valuearr.append(valueCheck)
+              valuearr.append(mispvalue)
+              for arritem in valuearr:
+                if arritem not in temparr:
+                  temparr.append(arritem)
+              client.set(str(item['value']), temparr, 2100)
+            else:
+              client.set(str(item['value']), 'misp-' + item['type'], 2100)
 
       except Exception as e:
         with open('/var/log/misppullLog.txt','a') as file:
-          file.write('{0} - mispFeed-script failed with error: {1} \n'.format(str(time.asctime()), str(e)))
+          file.write('{0} - mispFeed-script failed with error: {1} \n'.format(st                                                                                      r(time.asctime()), str(e)))
 
 
 if __name__=='__main__':
   mispToMemcache().run()
+
