1cfea6b2a85e6677c8c263e8c7d39c18f194901e
DennisLund <Dennis.l.christiansen@gmail.com>
2019-09-02 20:00:17 +0200

    Update mispFeed.py

--- memcacheScripts/mispFeed.py
+++ memcacheScripts/mispFeed.py
@@ -21,7 +21,7 @@ class mispToMemcache():
       headers={'Authorization':mispKey,'Accept':'application/json','Content-type                                                                                      ':'application/json'}
       data=json.dumps({"returnFormat":"json","type":dt,"tags":"Feed-%","to_ids":                                                                                      "yes","includeEventTags":"yes","includeContext":"yes"})
       try:
-        response = requests.post('https://192.168.0.13/attributes/restSearch',he                                                                                      aders=headers,data=data,verify=False)
+        response = requests.post('https://192.168.0.13/attributes/restSearch',headers=headers,data=data,verify=False)                                                                                     aders=headers,data=data,verify=False)
         data=response.json()
         if(data):
           for item in data["response"]["Attribute"]:
@@ -41,7 +41,7 @@ class mispToMemcache():
 
       except Exception as e:
         with open('/var/log/misppullLog.txt','a') as file:
-          file.write('{0} - mispFeed-script failed with error: {1} \n'.format(st                                                                                      r(time.asctime()), str(e)))
+          file.write('{0} - mispFeed-script failed with error: {1} \n'.format(str(time.asctime()),str(e)))                                                                                      r(time.asctime()), str(e)))
 
 
 if __name__=='__main__':
